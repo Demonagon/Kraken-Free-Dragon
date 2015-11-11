442c47144bcd8e06f7827a9ed57c76ea988f4870
Hayanno <nicolas@leotier.fr>
2015-11-11 16:33:55 +0100

    amélioration de la classe ChoiceContextMenu pour intégrer une liste de règle (List<Rule>) et un noeud de l'arbre (GraphicExpression)

--- FreeKraken/src/controller/PrimaryMouseEventManager.java
+++ FreeKraken/src/controller/PrimaryMouseEventManager.java
@@ -53,46 +53,7 @@ public class PrimaryMouseEventManager extends MouseEventManager {
 	public void onMousePressed() {
 		 group.setOnMousePressed(new EventHandler<MouseEvent>(){
 			 public void handle(MouseEvent event){
-				 	// on enl�ve 1/8 � la hauteur et la largeur pour avoir quelque chose qui ressemble � la vrai valeur
-	            	final double width =  getText().getLayoutBounds().getWidth() - (getText().getLayoutBounds().getWidth()/8);;
-	            	final double height = getText().getLayoutBounds().getHeight() - (getText().getLayoutBounds().getHeight() * 3/8);
-	            	
-	            	// on inverse le Y pour avoir une valeur positive
-	            	final double cursorY = - event.getY();
-	            	final double cursorX = event.getX();
-	            	
-	            	// le nombre de pixel � partir du bord qui d�crit la zone d�tect�
-	            	final int pixelDetectionAccuracy = 10;
-	            	String position = "Coin";
-	            	
-	            	// d�tecter le position de la souris au moment du drop
-	            	// la valeur de retour devra surement �tre stock� dans un enum mais
-	            	// je ne connais pas Java assez bien, on y r�fl�chira
-	            	
-	            	if(cursorX < pixelDetectionAccuracy
-	            	&& cursorY > pixelDetectionAccuracy
-	            	&& cursorY < height - pixelDetectionAccuracy)
-	            			position = "LEFT";
-	            	else if(cursorX > width - pixelDetectionAccuracy
-	            		 && cursorY > pixelDetectionAccuracy
-	            		 && cursorY < height - pixelDetectionAccuracy)
-	            			position = "RIGHT";
-	            	else if(cursorX > pixelDetectionAccuracy
-	            		 && cursorX < width - pixelDetectionAccuracy
-	            		 && cursorY > height - pixelDetectionAccuracy)
-	            			position = "TOP";
-	            	else if(cursorX > pixelDetectionAccuracy
-	            		 && cursorX < width - pixelDetectionAccuracy
-	            		 && cursorY < pixelDetectionAccuracy)
-	            			position = "BOTTOM";
-	            	
-	            	String dim 		= "(width: " + width + ", height: " + height + ")";
-	            	
-	            	String coord 	= "(x: " + cursorX + ", y: " + cursorY + ")";
-	            	
-	            	System.out.println(dim);
-	            	System.out.println(coord); // TODO DEL this lines
-	            	System.out.println(position);
+				 
 	            }
 	        });
 	}
