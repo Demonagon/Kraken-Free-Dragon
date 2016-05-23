80dcdf9d22d8a2d408a574795f4c64ed6b345bcb
Nibeldown <olivier.peirone@gmail.com>
2016-05-23 11:50:45 +0200

    implementation GraphicExpressionFactory pour LaTeX

--- FreeKraken/src/view/GraphicExpressionFactory.java
+++ FreeKraken/src/view/GraphicExpressionFactory.java
@@ -22,36 +22,38 @@ public interface GraphicExpressionFactory {
 	 * @param second : l'objet graphique correspondant au second sous noeud
 	 * @return l'objet graphique utilisateur
 	 */
-	public Object generateBinaryExpression(Expression expression, String type, Object first, Object second);
+	public String generateBinaryExpression(Expression expression, String type, Expression first, Expression second, String id);
 
 	/**
-	 * Génère un objet graphique.
-	 * @param expression : l'expression unaire à traduire
+	 * GÃ©nÃ¨re un objet graphique.
+	 * @param expression : l'expression unaire Ã  traduire
 	 * @param type : Son type
 	 * @param sub : l'objet graphique correspondant au sous noeud
 	 * @return l'objet graphique utilisateur
 	 */
-	public Object generateUnaryExpression(Expression expression, String type, Object sub);
+	public String generateUnaryExpression(Expression expression, String type, Expression sub, String id);
 
 	/**
-	 * Génère un objet graphique.
-	 * @param expression : l'expression primaire à traduire
+	 * GÃ©nÃ¨re un objet graphique.
+	 * @param expression : l'expression primaire Ã  traduire
 	 * @param type : Son type
 	 * @param name : Le nom de l'instance de l'objet primaire
 	 * @return l'objet graphique utilisateur
 	 */
-	public Object generatePrimaryExpression(Expression expression, String type, String name);
+	public String generatePrimaryExpression(Expression expression, String type, String name, String id);
 	
 	/**
-	 * Génère un objet graphique correspondant à une règle.
-	 * @param rule : la règle à traduire
+	 * GÃ©nÃ¨re un objet graphique correspondant Ã  une rÃ¨gle.
+	 * @param rule : la rÃ¨gle Ã  traduire
 	 * @return l'objet graphique utilisateur
 	 */
-	public Object generateRuleExpression(Rule rule);
+	
+	public String generateRuleExpression(Rule rule);
 	
 	/**
-	 * Fonction permettant à l'utilisateur d'initialiser d'éventuelles ressources.
-	 * Est appellée par KrakenTree en début de vie du logiciel.
+	 * Fonction permettant Ã  l'utilisateur d'initialiser d'Ã©ventuelles ressources.
+	 * Est appellÃ©e par KrakenTree en dÃ©but de vie du logiciel.
 	 */
+	
 	public void init();
 }
